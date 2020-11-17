d02eba77ba4fdb3e7134c0ae8953bf5ab7b03cdd
zheng2x2 <zheng@>
2020-11-17 22:53:27 +0900

    add generating 3 num class
    Added devided two method, so that one does only generating random number and the other one does repeat it and check the 3 nums are same

---
+++ src/main/java/com/zheng2x2/baseball/Main.java
@@ -0,0 +1,11 @@
+package com.zheng2x2.baseball;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GenerateNumber genNum = new GenerateNumber();
+        String answer = genNum.getThreeNumbers();
+
+    }
+}
